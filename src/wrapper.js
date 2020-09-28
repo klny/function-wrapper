a423db4b4b3752793bb81784d722a1c10726137e
Karol Nagy <karol.nagy.kn@gmail.com>
2020-09-28 11:04:38 +0200

    Conditional wrapping (#4)
    Added wrap-time and execution-time conditions to control wrapper's functionality.

--- src/wrapper.js
+++ src/wrapper.js
@@ -1,5 +1,20 @@
-function wrapper(func, before, after) {
+/**
+ * Wraps function "func" with "before" and "after" functions when "enabled".
+ * @param  {Function} func    Function to be wrapped
+ * @param  {Function} before  Function to be executed before wrapped function
+ * @param  {Function} after   Function to be executed after wrapped function
+ * @param  {Boolean}  wrap    Switch to enable/disable function wrapping on wrap-time (will return original function when false)
+ * @param  {Function} cond    Condition to skip wrapper functionality on function execution-time. Has access to arguments.
+ * @return {Function}         Wrapped function.
+ */
+function wrapper(func, before, after, wrap = true, cond = (args) => true) {
+  // return original function when wrapping is disabled
+  if (!wrap) return func;
+
   function f() {
+    // skip before/after function if not enabled
+    if (!cond(arguments)) return func.apply(this, arguments);
+
     // run before wrapped function
     const beforeResult = before(arguments);
 
